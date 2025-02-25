5e7ca7ac778ccd9917ec0650c96d872499863c95
potistudio <potistudio2022@gmail.com>
2025-02-26 06:41:11 +0900

    👍 search string

--- src-rust/src/main.rs
+++ src-rust/src/main.rs
@@ -1,3 +1,15 @@
+use std::io::Read;
+use bstr::ByteSlice;
+
 fn main() {
-    println!("Hello, world!");
+	// let mut aiFile = std::fs::File::open ("D:/Projects/Development/After Effects Scripts/overload/dist/Untitled_003.ai").unwrap();
+	// let mut buffer = bstr::BString::new ();
+
+	let mut buffer = bstr::BString::from(std::fs::read("D:/Projects/Development/After Effects Scripts/overload/dist/Untitled_003.ai").unwrap());
+
+	for start in buffer.find_iter("endobj") {
+		println!("{}", start);
+	}
+
+	println! ("{}", buffer.len());
 }
